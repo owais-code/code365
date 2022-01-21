fa87214a0c4737a5f9cf7760fea08778f2e575f9
Owais (Professor_011) <75789858+owais-code@users.noreply.github.com>
2022-01-22 02:26:04 +0530

    Update square_in_triangle.cpp

--- square_in_triangle.cpp
+++ square_in_triangle.cpp
@@ -1,3 +1,7 @@
+/*What is the maximum number of squares of size 2x2 that can be fit in a right angled isosceles triangle of base B.
+One side of the square must be parallel to the base of the isosceles triangle.
+Base is the shortest side of the triangle*/
+
 #include<iostream>
 using namespace std;
 
