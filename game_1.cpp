d4cf92996fea713abbff6c00a81be7380d470627
Owais (Professor_011) <75789858+owais-code@users.noreply.github.com>
2022-01-22 02:33:42 +0530

    Update and rename game.cpp to game_1.cpp

--- game_1.cpp
+++ game_1.cpp
@@ -1,3 +1,6 @@
+//https://www.codechef.com/problems/NUMGAME
+//Remember Game Theory, n: Winning case; n-1: loosing case .............
+
 #include<iostream>
 using namespace std;
 #define game(a) ((a)%2==0 ? printf("Alice") : printf("Bob"))
