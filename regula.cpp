ba01bd2da3ad749b094fbf713e9503b2760e207e
Owais (Professor_ẟ) <75789858+owais-code@users.noreply.github.com>
2022-01-24 01:37:27 +0530

    Update regula.cpp

--- regula.cpp
+++ regula.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #define min(a,b) ((a>b) ? b : a)
 #define max(a,b) ((a>b) ? a : b)
-
 using namespace std;
 
 double f(double x)
@@ -12,12 +11,15 @@ double f(double x)
 
 int regula_falsi(double a,double b)
 {
+  double x;
+  cout<<"Enter accuracy upto how much decimal places: ";
+  cin>>x;
   if(f(a)*f(b) >= 0)
   {
     cout << "\nIncorrect intervals chosen";
     return 0;
   }
-  double c =b, d = 0;
+  double c;
   int i = 1;
   while(true)
   {
@@ -25,7 +27,7 @@ int regula_falsi(double a,double b)
     cout << "\nIteration " << i << "\tRoot = " << c << endl;
     cout << a <<" "<< b << endl;
     i++;
-    if((c-a)<0.0001)
+    if((c-a)<pow(10, -(x+1)))
         {
             break;
         }
@@ -43,17 +45,19 @@ int regula_falsi(double a,double b)
       a = c;
     }
   }
-  cout << "\n\nThe root of the equation correct to 4 decimal places is " << c<<endl;
+  cout << "\n\nThe root of the equation correct to 4 decimal places is " <<c<<endl;
   return 0;
 }
 
 int main()
 {
   double c,d,a,b;
-  cout << "Enter the intervals a and b\n";
-  cin >> c >> d;
+  cout << "Enter the intervals"<<endl;
+  cout<<"a : "; cin>>c;
+  cout<<"b : "; cin>>d;
   a = min(c,d);
   b = max(c,d);
+  
   regula_falsi(a,b);
   return 0;
 }
