c3269c6129e1e988cca1e49620dfdff78c33cdf4
Owais (Professor_ẟ) <75789858+owais-code@users.noreply.github.com>
2022-01-25 02:23:57 +0530

    Add files via upload

---
+++ strin.py
@@ -0,0 +1,14 @@
+s = str(input())
+
+def sorts(s):
+    for i in range(0, len(s)):
+     s[i] = int(s)
+    s.sort()
+
+for i in range(0, len(s)):
+    if(s[i] == '+'):
+        print("+")
+    else:
+        s 
+
+print(s)        
