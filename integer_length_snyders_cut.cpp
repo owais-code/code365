08ea500840fa0154442a4b924e2770efd5553f37
Owais | Spidey <75789858+owais-code@users.noreply.github.com>
2022-03-20 01:08:47 +0530

    Create integer_length_snyders_cut.cpp

---
+++ integer_length_snyders_cut.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+    {
+        double n;
+        cout<<"Enter number: ";
+        cin>>n;
+        cout<<"Length of integer: "<<int(log10(n)+1);
+    }
